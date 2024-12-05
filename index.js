f8ea7dbff6b4982d363b83ce8c004580ed63be0f
Zelta780 <vinszstore098@gmail.com>
2024-12-05 11:48:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
+const BotName = 'Zelta'; 
+const instagram = 'mad4you_kbl'; 
+const aktif = '24 jam';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
             + 'FN:Agus\n' // Nama kamu
